8dd7bc54d1b1138022c07d89386e1415336cf7f4
fakumetal <faku.metal@gmail.com>
2024-03-20 17:50:17 -0300

    mantenimiento - facturas

---
+++ src/Components/Mantenimiento/TabsMantenimiento/DatosFacturasAFIP.jsx
@@ -0,0 +1,31 @@
+import eye from '../../../assets/eye.svg';  
+
+const datosFacturasAFIP = [
+  {
+    numeroOrden: '#001',
+    nombreApellido: 'Juan Pérez',
+    domicilio: 'Calle 123, Ciudad',
+    cuitCuilCdi: '20-12345678-9',
+    icon: eye,  
+  
+  },
+  {
+    numeroOrden: '#001',
+    nombreApellido: 'Juan Pérez',
+    domicilio: 'Calle 123, Ciudad',
+    cuitCuilCdi: '20-12345678-9',
+    icon: eye,
+    
+  },
+  {
+    numeroOrden: '#002',
+    nombreApellido: 'María López',
+    domicilio: 'Avenida Principal, Pueblo',
+    cuitCuilCdi: '27-98765432-1',
+    icon: eye,
+  
+  },
+ 
+];
+
+export default datosFacturasAFIP;
--- src/Components/Mantenimiento/TabsMantenimiento/FacturasAFIP.jsx
+++ src/Components/Mantenimiento/TabsMantenimiento/FacturasAFIP.jsx
@@ -1,33 +1,30 @@
 import React from 'react';
 import Table from 'react-bootstrap/Table';
-import datosFacturas from './DatosFacturas'; 
+import datosFacturasAFIP from './DatosFacturasAFIP'; 
  
-const Facturas = () => {
+const FacturasAFIP = () => {
   return (
     <div className='facturas-ctn'>
       <Table striped hover>
         <thead>
           <tr>
-            <th>Operacion</th>
+           
             <th>N° de orden</th>
-            <th>Técnico D.</th>
-            <th>Técnico T.</th>
-            <th>Cliente</th>
-            <th>Monto</th>
-            <th>Medio de pago</th>
+            <th>Nombre y Apellido</th>
+            <th>Domicilio</th>
+            <th>CUIT/CUIL/CDI</th>
             <th>Factura</th>
+        
           </tr>
         </thead>
         <tbody>
-          {datosFacturas.map((factura, index) => (
+          {datosFacturasAFIP.map((factura, index) => (
             <tr key={index}>
-              <td>{factura.operacion}</td>
               <td>{factura.numeroOrden}</td>
-              <td>{factura.tecnicoD}</td>
-              <td>{factura.tecnicoT}</td>
-              <td>{factura.cliente}</td>
-              <td>{factura.monto}</td>
-              <td>{factura.medioPago}</td>
+              <td>{factura.nombreApellido}</td>
+              <td>{factura.domicilio}</td>
+              <td>{factura.cuitCuilCdi}</td>
+          
               <td>
                 <img
                   src={factura.icon}
@@ -44,4 +41,4 @@ const Facturas = () => {
   );
 };
 
-export default Facturas;
+export default FacturasAFIP;
