29295c02aba358238ded2510885bbc88e2094156
NachoMaraude <maraudenacho@gmail.com>
2024-04-08 15:52:49 -0300

    LV-18 // Maquetacion y estilos de vista proveedores

--- src/Components/Grilla/Grilla.jsx
+++ src/Components/Grilla/Grilla.jsx
@@ -1,6 +1,6 @@
-import { useState } from 'react';
-import './Grilla.css';
-import PropTypes from 'prop-types';
+import { useState } from "react";
+import "./Grilla.css";
+import PropTypes from "prop-types";
 const Grilla = ({ columnas, elementos }) => {
   const [itemInventario, setitemInventario] = useState(-1);
 
@@ -10,32 +10,41 @@ const Grilla = ({ columnas, elementos }) => {
 
   return (
     <div>
-      <ul className='row p-0'>
+      <ul className="row p-0">
         {columnas.map((columna, index) => (
-          <li key={index} className='col'>
+          <li key={index} className="col columna-li">
             {columna} <span></span>
           </li>
         ))}
       </ul>
-      <ul className='grilla'>
+      <ul className="grilla">
         {elementos.map((item, index) => (
-          <div key={index} className='itemContainer'>
-            <ul className={`ulFlecha row mb-1 p-0 ${index % 2 === 0 ? 'bg-light' : ''}`}>
+          <div key={index} className="itemContainer">
+            <ul
+              className={`ulFlecha row mb-1 p-0 ${
+                index % 2 === 0 ? "bg-light" : ""
+              }`}
+            >
               {Object.entries(item).map(([key, valor], index) =>
                 Array.isArray(valor) ? (
-                  ''
+                  ""
                 ) : (
                   <li
                     key={index}
-                    className={`col text-center ${key === 'nOrden' ? 'valorItem' : ''}`}
+                    className={`col valor-li text-center ${
+                      key === "nOrden" ? "valorItem" : ""
+                    }`}
                   >
                     {valor}
                   </li>
                 )
               )}
               {item.lotes && (
-                <li className='valorItem'>
-                  <span onClick={() => toggleItemInventario(index)} className='flechaAbajo'></span>
+                <li className="valorItem">
+                  <span
+                    onClick={() => toggleItemInventario(index)}
+                    className="flechaAbajo"
+                  ></span>
                 </li>
               )}
             </ul>
@@ -43,9 +52,9 @@ const Grilla = ({ columnas, elementos }) => {
               <div>
                 <ul>
                   {item.lotes.map((lote, index) => (
-                    <li key={index} className='bg-primary-subtle col mb-1'>
-                      Proveedor {lote.proveedor} |Lote {lote.lote} | Orden {lote.orden} |{' '}
-                      {lote.unidadRestante} unidad restante
+                    <li key={index} className="bg-primary-subtle col mb-1">
+                      Proveedor {lote.proveedor} |Lote {lote.lote} | Orden{" "}
+                      {lote.orden} | {lote.unidadRestante} unidad restante
                     </li>
                   ))}
                 </ul>
--- src/Components/Ventas/TabsVentas/Proveedores.jsx
+++ src/Components/Ventas/TabsVentas/Proveedores.jsx
@@ -1,9 +1,150 @@
+import "./Proveedores.css";
+import Grilla from "../../Grilla/Grilla";
+import { Link } from "react-router-dom";
+
 const Proveedores = () => {
-    return (
+  const columnasStock = [
+    "Estado",
+    "Proveedor",
+    "Fecha",
+    "Importe",
+    "Caja",
+    "Op.",
+  ];
+  const itemsStock = [
+    {
+      estado: <strong style={{ color: "#40A63D" }}>Pago</strong>,
+      proveedor: "SALAZAR",
+      fecha: "27/01/2024",
+      importe: "$4325",
+      caja: 1,
+      op: "SALAZAR1234",
+      lotes: [
+        {
+          proveedor: "SALAZAR",
+          lote: "17/1/2-SALAZAR1234",
+          orden: "N/A",
+          unidadRestante: 1,
+        },
+        {
+          proveedor: "SALAZAR",
+          lote: "17/1/2-SALAZAR1234",
+          orden: "N/A",
+          unidadRestante: 1,
+        },
+      ],
+    },
+    {
+      estado: <strong style={{ color: "#40A63D" }}>Pago</strong>,
+      proveedor: "SALAZAR",
+      fecha: "27/01/2024",
+      importe: "$4325",
+      caja: 1,
+      op: "SALAZAR1234",
+      lotes: [
+        {
+          proveedor: "SALAZAR",
+          lote: "17/1/2-SALAZAR1234",
+          orden: "N/A",
+          unidadRestante: 1,
+        },
+      ],
+    },
+    {
+      estado: <strong style={{ color: "#40A63D" }}>Pago</strong>,
+      proveedor: "SALAZAR",
+      fecha: "27/01/2024",
+      importe: "$4325",
+      caja: 1,
+      op: "SALAZAR1234",
+      lotes: [
+        {
+          proveedor: "SALAZAR",
+          lote: "17/1/2-SALAZAR1234",
+          orden: "N/A",
+          unidadRestante: 1,
+        },
+      ],
+    },
+    {
+      estado: <strong style={{ color: "#D4674A" }}>Debe</strong>,
+      proveedor: "SALAZAR",
+      fecha: "27/01/2024",
+      importe: "$4325",
+      caja: 1,
+      op: "SALAZAR1234",
+      lotes: [
+        {
+          proveedor: "SALAZAR",
+          lote: "17/1/2-SALAZAR1234",
+          orden: "N/A",
+          unidadRestante: 1,
+        },
+      ],
+    },
+    {
+      estado: <strong style={{ color: "#40A63D" }}>Pago</strong>,
+      proveedor: "SALAZAR",
+      fecha: "27/01/2024",
+      importe: "$4325",
+      caja: 1,
+      op: "SALAZAR1234",
+      lotes: [
+        {
+          proveedor: "SALAZAR",
+          lote: "17/1/2-SALAZAR1234",
+          orden: "N/A",
+          unidadRestante: 1,
+        },
+      ],
+    },
+  ];
+
+  return (
+    <div className="proveedores-container">
       <div>
-       <h1>Proveedores</h1>
+        <div className="proveedores-heading mb-4">
+          <h1>Pagos a proveedores</h1>
+        </div>
+        <div className="proveedores-top-box">
+          <div>
+            <select className="proveedores-select">
+              <option value="">Proveedores</option>
+              {/* 
+            <button className="proveedores-button-arrow">▼</button> */}
+            </select>
+          </div>
+          <div>
+            <input
+              className="proveedores-input"
+              type="text"
+              placeholder="dd/mm/aaaa"
+            />
+            <button className="proveedores-button-search">🔍︎</button>
+          </div>
+
+          <div>
+            <h4>Saldo pendiente a la fecha: -$6565</h4>
+          </div>
+        </div>
+        <div className="proveedores-button-box">
+          <Link to="/cargarFactura">
+            <button>Cargar factura</button>
+          </Link>
+          <Link to="/gastos">
+            <button>Cargar gasto</button>
+          </Link>
+          <Link to="/addLoteExcel">
+            <button>Recibir lote</button>
+          </Link>
+        </div>
       </div>
-    );
-  }
-  
-  export default Proveedores;
+      <div className="proveedores-excel-wrapper">
+        <h2>Pagos efectuados</h2>
+        <Grilla columnas={columnasStock} elementos={itemsStock} />
+      </div>
+    </div>
+  );
+};
+
+export default Proveedores;
