0df3127460007617e78969ec256507b095d56c04
facuzero <facuzero_95@hotmail.com>
2024-04-15 17:41:27 -0300

    agrego imagen y estilos

--- src/Components/Ventas/AddLoteExcel.jsx
+++ src/Components/Ventas/AddLoteExcel.jsx
@@ -1,8 +1,10 @@
+import PropTypes from 'prop-types';
 import Header from '../Header/Header';
-import GrillaEditStock from './EditarStockRepuestos/GrillaEditStock';
+import trash from '../../images/trash.webp';
+import { useState } from 'react';
 
-const AddLoteExcel = () => {
-  const itemsStock = [
+const AddLoteExcel = ({ columnas, elementos }) => {
+  elementos = [
     {
       nombre: 'NombreAbc123',
       id: 4366,
@@ -34,6 +36,18 @@ const AddLoteExcel = () => {
       disponibles: 2,
     },
   ];
+  columnas = ['Nombre', 'ID', 'Precio', 'Disponibles'];
+  const [items, setItems] = useState(elementos);
+  const [itemsToDelete, setItemsToDelete] = useState([]);
+  const handleDelete = (index) => {
+    if (items[index]) {
+      const nuevosItems = items.filter((elemento) => elemento !== items[index]);
+      setItems(nuevosItems);
+    } else {
+      setItemsToDelete(items[index]);
+      console.log(itemsToDelete);
+    }
+  };
   return (
     <div>
       <Header text='Cargar lote desde Excel' />
@@ -42,13 +56,53 @@ const AddLoteExcel = () => {
         <input type='file' name='' id='' />
       </div>
       <div>
-        <GrillaEditStock
-          columnas={['Nombre', 'ID', 'Proveedor', 'Precio', 'Disponiles', 'Fecha', 'Orden']}
-          elementos={itemsStock}
-        />
+        <div>
+          <ul className='row p-0 text-center'>
+            {columnas.map((columna, index) => (
+              <li key={index} className='col'>
+                {columna} <span></span>
+              </li>
+            ))}
+          </ul>
+          <ul className='grilla'>
+            {items.map((item, index) => (
+              <div key={index} className='itemContainer'>
+                <ul className={`ulItem row mb-1 p-0 ${index % 2 === 0 ? 'bg-light' : ''}`}>
+                  {Object.entries(item).map(([, valor], index) => (
+                    <li key={index} className={`col text-center`}>
+                      {valor}
+                    </li>
+                  ))}
+                  <li className='d-flex justify-content-end position-relative '>
+                    <div className='d-flex'>
+                      <h1 className='borrar signo' onClick={() => handleDelete(index)}>
+                        +
+                      </h1>
+                      <h1 className='signo'>+</h1>
+                      <img src={trash} alt='editar' className='imgEditar' />
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            ))}
+          </ul>
+          <div className='d-flex justify-content-end'>
+            <button
+              className='rounded-pill bg-info text-white botonEliminar'
+              onClick={handleDelete}
+            >
+              Eliminiar
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
 };
 
 export default AddLoteExcel;
+
+AddLoteExcel.propTypes = {
+  columnas: PropTypes.array,
+  elementos: PropTypes.array,
+};
