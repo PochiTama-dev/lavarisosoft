3867096e3cc5ea22e749c7bbdc04bfc90d0a1b90
facuzero <facuzero_95@hotmail.com>
2024-05-05 16:23:30 -0300

    ajuste de logica

--- src/pages/Agenda/Agenda.jsx
+++ src/pages/Agenda/Agenda.jsx
@@ -1,12 +1,10 @@
 import Header from '../../Components/Header/Header.jsx';
 import Calendario from './Calendario.jsx';
 const Agenda = () => {
-  const fechaActual = new Date();
-  console.log(fechaActual.getMonth());
   return (
     <div>
       <Header text={'Agenda'} />
-      <Calendario dia={fechaActual.getDate()} mes={fechaActual.getMonth() + 1} />
+      <Calendario />
     </div>
   );
 };
