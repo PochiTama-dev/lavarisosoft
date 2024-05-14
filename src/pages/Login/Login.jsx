0570c6d85661cc328b94b76e9b297d7d9475dddb
facuzero <facuzero_95@hotmail.com>
2024-05-14 17:11:17 -0300

    ahora si se vé bien y con funcionalidad

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,11 @@
+import { useCustomContext } from '../../hooks/context';
 import imageLogin from '../../images/imageLogin.webp';
 import './Login.css';
+
 const Login = () => {
+  const { handleNavigate } = useCustomContext();
   return (
-    <div className='container d-flex bg-primary'>
+    <div className='d-flex bg-primary loginContainer'>
       <aside className='container-sm row align-items-center my-5'>
         <div>
           <h1 className='text-white'>LavaRiso</h1>
@@ -11,25 +14,15 @@ const Login = () => {
         <h3>¡Bienvenido de vuelta!</h3>
         <span>Credenciales requeridas</span>
         <form action='' method='post'>
-          <input
-            className='bg-white text-black rounded my-2'
-            type='email'
-            name=''
-            id=''
-            placeholder='E-mail'
-          />
-          <input
-            className='bg-white text-black rounded my-2'
-            type='password'
-            name=''
-            id=''
-            placeholder='Contraseña'
-          />
+          <input className='bg-white text-black rounded my-2' type='email' name='' id='' placeholder='E-mail' />
+          <input className='bg-white text-black rounded my-2' type='password' name='' id='' placeholder='Contraseña' />
         </form>
-        <button className='loginButton btn text-white'>Iniciar sesión</button>
+        <button className='loginButton btn text-white' onClick={() => handleNavigate('menu')}>
+          Iniciar sesión
+        </button>
       </aside>
-      <aside>
-        <img className='img-fluid' src={imageLogin} alt='image LavaRiso' />
+      <aside className='asideImg'>
+        <img className='imgLogin' src={imageLogin} alt='image LavaRiso' />
       </aside>
     </div>
   );
