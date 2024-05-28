d016d5a4acc1de1bb9db91492c9fbfeb66bf029e
NachoMaraude <maraudenacho@gmail.com>
2024-05-28 18:18:41 -0300

    Correcion de estilos de inventario

--- src/Components/Ventas/EditarStockRepuestos/EditarStockRepuestos.jsx
+++ src/Components/Ventas/EditarStockRepuestos/EditarStockRepuestos.jsx
@@ -1,52 +1,52 @@
-import Header from '../../Header/Header';
-import './EditarStockRepuestos.css';
-import GrillaEditStock from './GrillaEditStock';
+import Header from "../../Header/Header";
+import "./EditarStockRepuestos.css";
+import GrillaEditStock from "./GrillaEditStock";
 const EditarStockRespuestos = () => {
   const itemsStock = [
     {
-      nombre: 'NombreAbc123',
+      nombre: "NombreAbc123",
       id: 4366,
       precio: 2330,
       disponibles: 2,
     },
     {
-      nombre: 'NombreAbc456',
+      nombre: "NombreAbc456",
       id: 43365,
       precio: 2330,
       disponibles: 2,
     },
     {
-      nombre: 'NombreAbc789',
+      nombre: "NombreAbc789",
       id: 435,
       precio: 2330,
       disponibles: 2,
     },
     {
-      nombre: 'NombreAbc159',
+      nombre: "NombreAbc159",
       id: 3165,
       precio: 2330,
       disponibles: 2,
     },
     {
-      nombre: 'NombreAbc987',
+      nombre: "NombreAbc987",
       id: 9894,
       precio: 2330,
       disponibles: 2,
     },
   ];
   return (
-    <div>
-      <Header text='Editar stock de repuestos' />
-      <div className='stockContainer'>
+    <div className="stockContainer">
+      <Header text="Editar stock de repuestos" />
+      <div>
         <h1>Editar Stock</h1>
         <div>
-          <input className='caja-input' type='text' placeholder='Buscar...' />
-          <button className='caja-button-search'>🔍︎</button>
+          <input className="caja-input" type="text" placeholder="Buscar..." />
+          <button className="caja-button-search">🔍︎</button>
         </div>
       </div>
 
       <GrillaEditStock
-        columnas={['Nombre', 'ID', 'Precio', 'Disponibles', '']}
+        columnas={["Nombre", "ID", "Precio", "Disponibles", ""]}
         elementos={itemsStock}
       />
     </div>
--- src/Components/Ventas/EditarStockRepuestos/GrillaEditStock.jsx
+++ src/Components/Ventas/EditarStockRepuestos/GrillaEditStock.jsx
@@ -1,19 +1,125 @@
-import '../../Grilla/Grilla.css';
-import PropTypes from 'prop-types';
-import editar from '../../../images/editar2.webp';
-import { useState } from 'react';
-import { useCustomContext } from '../../../hooks/context.jsx';
+// import "../../Grilla/Grilla.css";
+// import PropTypes from "prop-types";
+// import editar from "../../../images/editar2.webp";
+// import { useState } from "react";
+// import { useCustomContext } from "../../../hooks/context.jsx";
+// const GrillaEditStock = ({ columnas, elementos }) => {
+//   const { handleEdit } = useCustomContext();
+//   const [checkboxStates, setCheckboxStates] = useState(
+//     elementos.map(() => false)
+//   );
+//   const [items, setItems] = useState(elementos);
+
+//   const handleDelete = (index) => {
+//     if (items[index]) {
+//       const nuevosItems = items.filter((elemento) => elemento !== items[index]);
+//       setItems(nuevosItems);
+//     } else {
+//       const nuevosElementos = items.filter(
+//         (elemento, index) => !checkboxStates[index]
+//       );
+//       setItems(nuevosElementos);
+//       setCheckboxStates(items.map(() => false));
+//     }
+//   };
+
+//   const handleChecked = (index) => {
+//     const newCheckboxStates = [...checkboxStates];
+//     newCheckboxStates[index] = !newCheckboxStates[index];
+//     setCheckboxStates(newCheckboxStates);
+//   };
+
+//   return (
+//     <div>
+//       <ul className="row p-0 text-center">
+//         {columnas.map((columna, index) => (
+//           <li key={index} className="col">
+//             {columna} <span></span>
+//           </li>
+//         ))}
+//       </ul>
+//       <ul className="grilla">
+//         {items.map((item, index) => (
+//           <div key={index} className="itemContainer">
+//             <ul
+//               className={`ulFlecha row mb-1 p-0 ${
+//                 index % 2 === 0 ? "bg-light" : ""
+//               }`}
+//             >
+//               {Object.entries(item).map(([, valor], index) => (
+//                 <li key={index} className={`col text-center`}>
+//                   {valor}
+//                 </li>
+//               ))}
+//               <li className="col">
+//                 <div className="d-flex align-items-center">
+//                   <img
+//                     src={editar}
+//                     alt="editar"
+//                     className="imgEditar"
+//                     onClick={() => handleEdit(item)}
+//                   />
+//                   <span
+//                     className="borrar signo"
+//                     onClick={() => handleDelete(index)}
+//                   >
+//                     +
+//                   </span>
+//                   <span className="signo">+</span>
+//                   <input
+//                     type="checkbox"
+//                     name=""
+//                     id=""
+//                     checked={checkboxStates[index]}
+//                     onChange={() => handleChecked(index)}
+//                   />
+//                 </div>
+//               </li>
+//             </ul>
+//           </div>
+//         ))}
+//       </ul>
+//       <div className="d-flex justify-content-end">
+//         <button
+//           className="rounded-pill bg-info text-white botonEliminar"
+//           onClick={handleDelete}
+//         >
+//           Eliminar
+//         </button>
+//       </div>
+//     </div>
+//   );
+// };
+// export default GrillaEditStock;
+
+// GrillaEditStock.propTypes = {
+//   columnas: PropTypes.array.isRequired,
+//   elementos: PropTypes.array.isRequired,
+// };
+
+import "../../Grilla/Grilla.css";
+import PropTypes from "prop-types";
+import editar from "../../../images/editar2.webp";
+import { useState } from "react";
+import { useCustomContext } from "../../../hooks/context.jsx";
+
 const GrillaEditStock = ({ columnas, elementos }) => {
   const { handleEdit } = useCustomContext();
-  const [checkboxStates, setCheckboxStates] = useState(elementos.map(() => false));
+  const [checkboxStates, setCheckboxStates] = useState(
+    elementos.map(() => false)
+  );
   const [items, setItems] = useState(elementos);
+  const [orderBy, setOrderBy] = useState(null);
+  const [orderAsc, setOrderAsc] = useState(true);
 
   const handleDelete = (index) => {
     if (items[index]) {
       const nuevosItems = items.filter((elemento) => elemento !== items[index]);
       setItems(nuevosItems);
     } else {
-      const nuevosElementos = items.filter((elemento, index) => !checkboxStates[index]);
+      const nuevosElementos = items.filter(
+        (elemento, index) => !checkboxStates[index]
+      );
       setItems(nuevosElementos);
       setCheckboxStates(items.map(() => false));
     }
@@ -25,40 +131,77 @@ const GrillaEditStock = ({ columnas, elementos }) => {
     setCheckboxStates(newCheckboxStates);
   };
 
+  const handleSort = (columnName) => {
+    if (orderBy === columnName) {
+      setOrderAsc((prevOrderAsc) => !prevOrderAsc);
+    } else {
+      setOrderBy(columnName);
+      setOrderAsc(true);
+    }
+  };
+
+  const sortedItems = [...items].sort((a, b) => {
+    if (!orderBy) return 0;
+    if (orderAsc) {
+      return a[orderBy] > b[orderBy] ? 1 : -1;
+    } else {
+      return a[orderBy] < b[orderBy] ? 1 : -1;
+    }
+  });
+
   return (
     <div>
-      <ul className='row p-0 text-center'>
+      <ul className="row p-0 text-center">
         {columnas.map((columna, index) => (
-          <li key={index} className='col'>
-            {columna} <span></span>
+          <li
+            key={index}
+            className="col"
+            onClick={() => handleSort(columna.toLowerCase())}
+            style={{
+              cursor: "pointer",
+              fontWeight: "bold",
+              fontSize: "large",
+              marginTop: "2.5%",
+            }}
+          >
+            {columna}{" "}
+            {orderBy === columna.toLowerCase() && (orderAsc ? "▲" : "▼")}
           </li>
         ))}
       </ul>
-      <ul className='grilla'>
-        {items.map((item, index) => (
-          <div key={index} className='itemContainer'>
-            <ul className={`ulFlecha row mb-1 p-0 ${index % 2 === 0 ? 'bg-light' : ''}`}>
+      <ul className="grilla">
+        {sortedItems.map((item, index) => (
+          <div key={index} className="itemContainer aling-items-center">
+            <ul
+              style={{ fontWeight: "500", height: "7vh" }}
+              className={`ulFlecha row p-0 ${
+                index % 2 === 0 ? "bg-light" : ""
+              }`}
+            >
               {Object.entries(item).map(([, valor], index) => (
                 <li key={index} className={`col text-center`}>
                   {valor}
                 </li>
               ))}
-              <li className='col'>
-                <div className='d-flex'>
+              <li className="col">
+                <div className="d-flex align-items-center">
                   <img
                     src={editar}
-                    alt='editar'
-                    className='imgEditar'
+                    alt="editar"
+                    className="imgEditar"
                     onClick={() => handleEdit(item)}
                   />
-                  <h1 className='borrar signo' onClick={() => handleDelete(index)}>
+                  <span
+                    className="borrar signo"
+                    onClick={() => handleDelete(index)}
+                  >
                     +
-                  </h1>
-                  <h1 className='signo'>+</h1>
+                  </span>
+                  <span className="signo">+</span>
                   <input
-                    type='checkbox'
-                    name=''
-                    id=''
+                    type="checkbox"
+                    name=""
+                    id=""
                     checked={checkboxStates[index]}
                     onChange={() => handleChecked(index)}
                   />
@@ -68,17 +211,21 @@ const GrillaEditStock = ({ columnas, elementos }) => {
           </div>
         ))}
       </ul>
-      <div className='d-flex justify-content-end'>
-        <button className='rounded-pill bg-info text-white botonEliminar' onClick={handleDelete}>
-          Eliminiar
+      <div className="d-flex justify-content-end">
+        <button
+          className="rounded-pill bg-info text-white botonEliminar"
+          onClick={handleDelete}
+        >
+          Eliminar
         </button>
       </div>
     </div>
   );
 };
-export default GrillaEditStock;
 
 GrillaEditStock.propTypes = {
   columnas: PropTypes.array.isRequired,
   elementos: PropTypes.array.isRequired,
 };
+
+export default GrillaEditStock;
