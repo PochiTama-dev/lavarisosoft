8ca6eb7e3989aa622366c2d68c60371029ccef7d
facuzero <facuzero_95@hotmail.com>
2024-05-28 17:09:38 -0300

    ajusto tamaños de login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -8,16 +8,16 @@ const Login = () => {
     <div className='d-flex bg-primary loginContainer'>
       <aside className='container-sm row align-items-center my-5'>
         <div>
-          <h1 className='text-white'>LavaRiso</h1>
-          <h5 className='text-white'>Servicio técnico de lavarropas a domicilio</h5>
+          <h1 className='text-white titleLogin'>LavaRiso</h1>
+          <h5 className='text-white subtitleLogin'>Servicio técnico de lavarropas a domicilio</h5>
         </div>
-        <h3>¡Bienvenido de vuelta!</h3>
-        <span>Credenciales requeridas</span>
-        <form action='' method='post'>
-          <input className='bg-white text-black rounded my-2' type='email' name='' id='' placeholder='E-mail' />
-          <input className='bg-white text-black rounded my-2' type='password' name='' id='' placeholder='Contraseña' />
+        <h3 className='messageTitle text-center'>¡Bienvenido de vuelta!</h3>
+        <span className='messageTitle'>Credenciales requeridas</span>
+        <form action='' method='post' className='d-flex flex-column'>
+          <input className='bg-white text-black rounded-pill my-2 inputsLogin' type='email' name='' id='' placeholder='E-mail' />
+          <input className='bg-white text-black rounded-pill my-2 inputsLogin' type='password' name='' id='' placeholder='Contraseña' />
         </form>
-        <button className='loginButton btn text-white' onClick={() => handleNavigate('menu')}>
+        <button className='loginButton btn text-white rounded-pill' onClick={() => handleNavigate('menu')}>
           Iniciar sesión
         </button>
       </aside>
