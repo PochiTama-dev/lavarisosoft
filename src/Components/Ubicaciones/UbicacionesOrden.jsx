1c5aecc46f4f6d564c494461aa26d1ad8333661d
matidiaz032 <maty_032@hotmail.es>
2024-07-17 23:39:32 -0300

    vista confirmar orden vacia

--- src/Components/Ubicaciones/UbicacionesOrden.jsx
+++ src/Components/Ubicaciones/UbicacionesOrden.jsx
@@ -1,11 +1,14 @@
+import { useEffect, useState } from 'react';
 import Header from "../Header/Header";
 import { useLocation } from "react-router-dom";
+import { ordenes, guardarOrden } from "../../services/ordenesService";
 
 import "./Ubicaciones.css";
 
 const UbicacionesOrden = () => {
   const location = useLocation();
   const { selectedTechnician, selectedClient } = location.state || {};
+  const [nroNuevaOrden, setNroNuevaOrden] = useState(0)
 
   const separarDireccion = (direccion) => {
     const partes = direccion.split(" ");
@@ -16,19 +19,69 @@ const UbicacionesOrden = () => {
 
   const { calle, altura } = separarDireccion(selectedClient.direccion);
 
+  const fetchOrdenes = async () => {
+    try {
+      const data = await ordenes();
+      if (data.length > 0) {
+        console.log(`Se encontraron una lista con ${data.length} ordenes!!`);
+        const entries = Object.entries(data)
+        const [last] = entries.slice(-1)
+        const nroOrden = last[1].id + 1; //Tomo el ultimo id de las ordenes cargadas y le sumo 1 
+        setNroNuevaOrden(nroOrden);
+      } else {
+        console.log('Aún no se registra ningúna orden...');
+      }
+    } catch (error) {
+      console.error("Error, no se encontraron ordenes en la base de datos....", error);
+    }
+  };
+
+  const handleSubmit = async () => {
+    if (selectedClient) {
+      const orden = {
+        numero_orden: nroNuevaOrden,
+        id_cliente: selectedClient.id,
+        id_empleado: selectedTechnician.id,
+        id_tipo_estado: 4,
+        id_tipo_cierre_extendido: null,
+        equipo: "",
+        marca: "",
+        modelo: "",
+        antiguedad: 0,
+        diagnostico: null,
+        motivo: null,
+      };
+      const ordenGuardada = await guardarOrden(orden);
+      console.log('ordenGuardada:', ordenGuardada);
+      if (ordenGuardada) {
+        alert("Orden guardada con éxito");
+        console.log("Orden completa guardada con éxito!!!");
+      } else {
+        alert("Error al guardar orden");
+        console.log("Error al guardar la orden completa...");
+      }
+    }
+  };
+
+  useEffect(() => {
+    fetchOrdenes();
+  })
+
   return (
     <div className="ventas-container">
       <Header text="Ubicaciones"></Header>
       <div className="row w-100 p-5 mt-5">
         <h2 className="pt-3 mb-5 mx-4 feedback-containers-heading">
-          Confirmar orden no. #{selectedClient.Ordenes[0]?.id}
+          Confirmar orden no. #{nroNuevaOrden}
         </h2>
         <div className="col-5">
           <div className="container-lists">
             <h2 className="px-3 pt-3 feedback-containers-heading">Cliente</h2>
             <div className="scrollable-container-top">
               <div className="feedback-tecnicos-container align-items-center">
-                <h4 className="feedback-tecnicos-heading">{selectedClient.nombre} {selectedClient.apellido}</h4>
+                <h4 className="feedback-tecnicos-heading">
+                  {selectedClient.nombre} {selectedClient.apellido}
+                </h4>
               </div>
               <div className="feedback-tecnicos-container align-items-center">
                 <h4 className="feedback-tecnicos-heading">
@@ -36,15 +89,15 @@ const UbicacionesOrden = () => {
                 </h4>
               </div>
               <div className="feedback-tecnicos-container align-items-center">
-                <h4 className="feedback-tecnicos-heading">
-                  Calle: {calle}
-                </h4>
+                <h4 className="feedback-tecnicos-heading">Calle: {calle}</h4>
               </div>
               <div className="feedback-tecnicos-container align-items-center">
                 <h4 className="feedback-tecnicos-heading">Altura: {altura} </h4>
               </div>
               <div className="feedback-tecnicos-container align-items-center">
-                <h4 className="feedback-tecnicos-heading">Localidad: {selectedClient.ubicacion}</h4>
+                <h4 className="feedback-tecnicos-heading">
+                  Localidad: {selectedClient.ubicacion}
+                </h4>
               </div>
             </div>
           </div>
@@ -59,7 +112,10 @@ const UbicacionesOrden = () => {
                 <table className="table">
                   <tbody>
                     <tr className="row-even">
-                      <td>{selectedTechnician.nombre} {selectedTechnician.apellido}</td>
+                      <td>
+                        {selectedTechnician.nombre}{" "}
+                        {selectedTechnician.apellido}
+                      </td>
                       <td>Legajo: {selectedTechnician.legajo}</td>
                       <td>Técnico a domicilio</td>
                     </tr>
@@ -68,6 +124,9 @@ const UbicacionesOrden = () => {
               </div>
             </div>
           </div>
+        </div>
+        <div className='d-flex justify-content-center'>
+          <button className='bg-info rounded-pill text-white' onClick={handleSubmit}>Confirmar</button>
         </div>
       </div>
     </div>
