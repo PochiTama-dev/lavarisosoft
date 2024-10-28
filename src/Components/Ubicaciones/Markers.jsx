a7d79fafc13cd5772adcdbfd4c69c1f4c9e393b5
facuzero <facuzero_95@hotmail.com>
2024-10-28 14:31:32 -0300

    temporizador de tecnico cerca del cliente

---
+++ src/Components/Ubicaciones/Markers.jsx
@@ -0,0 +1,75 @@
+import { Marker, Popup } from 'react-leaflet';
+import { Icon } from 'leaflet';
+import markerIcon from '../../assets/marker.png';
+import clientIcon from '../../assets/man.webp';
+import 'leaflet/dist/leaflet.css';
+import PropTypes from 'prop-types';
+
+const clienteIcono = new Icon({
+  iconUrl: clientIcon,
+  iconSize: [30, 30],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -35],
+});
+
+const myIcon = new Icon({
+  iconUrl: markerIcon,
+  iconSize: [30, 30],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -35],
+});
+export const TechnicianMarker = ({ tecnico, onTechnicianSelect }) => (
+  <Marker position={[tecnico.latitud, tecnico.longitud]} icon={myIcon}>
+    <Popup>
+      <div className='popup-tecnico'>
+        <img src={`https://via.placeholder.com/50`} alt='Technician' />
+        <div className='popup-content'>
+          <h4>{tecnico.nombre}</h4>
+          <p>{tecnico.telefono}</p>
+        </div>
+      </div>
+      <div className='popup-tecnico-button'>
+        <button onClick={onTechnicianSelect}>
+          <svg xmlns='http://www.w3.org/2000/svg' width='22' height='22' fill='currentColor' className='bi bi-arrow-right'>
+            <path fillRule='evenodd' d='M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8' />
+          </svg>
+        </button>
+      </div>
+    </Popup>
+  </Marker>
+);
+
+export const ClientMarker = ({ cliente, handleOpenModalWithClientData, handleCoordinates }) => (
+  <Marker position={[cliente.latitud, cliente.longitud]} icon={clienteIcono} eventHandlers={{ click: () => handleCoordinates(cliente) }}>
+    <Popup>
+      <div className='popup-tecnico'>
+        <img src={`https://via.placeholder.com/50`} alt='Client' />
+        <div className='popup-content'>
+          <h4>{cliente.nombre}</h4>
+          <p>{cliente.telefono}</p>
+        </div>
+      </div>
+      <div className='popup-tecnico-button'>
+        <button onClick={() => handleOpenModalWithClientData(cliente)}>Ver detalles</button>
+      </div>
+    </Popup>
+  </Marker>
+);
+
+// Definir los PropTypes comunes
+const locationPropTypes = PropTypes.shape({
+  latitud: PropTypes.string.isRequired,
+  longitud: PropTypes.string.isRequired,
+  nombre: PropTypes.string.isRequired,
+  telefono: PropTypes.string.isRequired,
+}).isRequired;
+
+TechnicianMarker.propTypes = {
+  tecnico: locationPropTypes, // Usando el conjunto común de PropTypes
+  onTechnicianSelect: PropTypes.func.isRequired,
+};
+ClientMarker.propTypes = {
+  cliente: locationPropTypes, // Usando el conjunto común de PropTypes
+  handleOpenModalWithClientData: PropTypes.func.isRequired,
+  handleCoordinates: PropTypes.func.isRequired,
+};
--- src/Components/Ubicaciones/UbicacionesOrden.jsx
+++ src/Components/Ubicaciones/UbicacionesOrden.jsx
@@ -1,18 +1,21 @@
-import { useEffect, useState } from "react";
-import Header from "../Header/Header";
-import { useLocation } from "react-router-dom";
-import { ordenes, guardarOrden } from "../../services/ordenesService";
+import { useEffect, useState } from 'react';
+import Header from '../Header/Header';
+import { useLocation } from 'react-router-dom';
+import { ordenes, guardarOrden } from '../../services/ordenesService';
 
-import "./Ubicaciones.css";
+import './Ubicaciones.css';
+import { useCustomContext } from '../../hooks/context';
 
 const UbicacionesOrden = () => {
   const location = useLocation();
-  const { selectedTechnician, selectedClient } = location.state || {};
-  const [nroNuevaOrden, setNroNuevaOrden] = useState(0);
+  const { selectedTechnician } = location.state || {};
+  const [orders, setOrders] = useState([]);
+  const [selectedOrder, setSelectedOrder] = useState([]);
+  const { handleNavigate } = useCustomContext();
 
-  const separarDireccion = (direccion) => {
-    if (!direccion || typeof direccion !== "string") {
-      return { calle: "", altura: "" };
+  /* const separarDireccion = (direccion) => {
+    if (!direccion || typeof direccion !== 'string') {
+      return { calle: '', altura: '' };
     }
 
     const regex = /(.*?)(\d+)\s*$/;
@@ -24,32 +27,27 @@ const UbicacionesOrden = () => {
       return { calle, altura };
     }
 
-    return { calle: direccion, altura: "" };
+    return { calle: direccion, altura: '' };
   };
 
-  const { calle, altura } = separarDireccion(selectedClient.direccion);
+  const { calle, altura } = separarDireccion(selectedClient.direccion); */
 
   const fetchOrdenes = async () => {
     try {
       const data = await ordenes();
       if (data.length > 0) {
-        console.log(`Se encontraron una lista con ${data.length} ordenes!!`);
-        const entries = Object.entries(data);
-        const [last] = entries.slice(-1);
-        const nroOrden = last[1].id + 1; //Tomo el ultimo id de las ordenes cargadas y le sumo 1
-        setNroNuevaOrden(nroOrden);
+        const ordersEmployee = data.filter((orden) => orden.Empleado.id === selectedTechnician.id);
+        const ordersAproved = ordersEmployee.filter((order) => order.TiposEstado.id === 1);
+        setOrders(ordersAproved); //Todas las ordenes del Tecnico que estan aprobadas
       } else {
-        console.log("Aún no se registra ningúna orden...");
+        console.log('Aún no se registra ningúna orden...');
       }
     } catch (error) {
-      console.error(
-        "Error, no se encontraron ordenes en la base de datos....",
-        error
-      );
+      console.error('Error, no se encontraron ordenes en la base de datos....', error);
     }
   };
 
-  const handleSubmit = async () => {
+  /* const handleSubmit = async () => {
     if (selectedClient) {
       const orden = {
         numero_orden: nroNuevaOrden,
@@ -57,77 +55,79 @@ const UbicacionesOrden = () => {
         id_empleado: selectedTechnician.id,
         id_tipo_estado: 4,
         id_tipo_cierre_extendido: null,
-        equipo: "",
-        marca: "",
-        modelo: "",
+        equipo: '',
+        marca: '',
+        modelo: '',
         antiguedad: 0,
         diagnostico: null,
         motivo: null,
       };
       const ordenGuardada = await guardarOrden(orden);
-      console.log("ordenGuardada:", ordenGuardada);
+      console.log('ordenGuardada:', ordenGuardada);
       if (ordenGuardada) {
-        alert("Orden guardada con éxito");
-        console.log("Orden completa guardada con éxito!!!");
+        alert('Orden guardada con éxito');
+        console.log('Orden completa guardada con éxito!!!');
       } else {
-        alert("Error al guardar orden");
-        console.log("Error al guardar la orden completa...");
+        alert('Error al guardar orden');
+        console.log('Error al guardar la orden completa...');
       }
     }
-  };
+  }; */
 
+  const handleSelectOrder = async (ordenId) => {
+    const ordenToSelect = orders.find((orden) => orden.id === ordenId);
+    setSelectedOrder(ordenToSelect);
+  };
+  const handleSocket = () => {
+    handleNavigate('/location');
+  };
   useEffect(() => {
     fetchOrdenes();
-  });
+    console.log(selectedOrder);
+  }, [selectedOrder]);
 
   return (
-    <div className="ventas-container">
-      <Header text="Ubicaciones"></Header>
-      <div className="row w-100 p-5 mt-5">
-        <h2 className="pt-3 mb-5 mx-4 feedback-containers-heading">
-          Confirmar orden no. #{nroNuevaOrden}
-        </h2>
-        <div className="col-5">
-          <div className="container-lists">
-            <h2 className="px-3 pt-3 feedback-containers-heading">Cliente</h2>
-            <div className="scrollable-container-top">
-              <div className="feedback-tecnicos-container align-items-center">
-                <h4 className="feedback-tecnicos-heading">
-                  {selectedClient.nombre} {selectedClient.apellido}
-                </h4>
+    <div className='ventas-container'>
+      <Header text='Ubicaciones'></Header>
+      <div className='row w-100 p-5 mt-5'>
+        {/* <h2 className='pt-3 mb-5 mx-4 feedback-containers-heading'>Nueva orden no.#{nroNuevaOrden}</h2> */}
+        <div className='row'>
+          <h2 className='px-3 pt-3 feedback-containers-heading'>Seleccionar orden para trabajar</h2>
+          {orders &&
+            orders.map((orden) => (
+              <div key={orden.id} className={`container-lists col ${selectedOrder.id === orden.id ? 'bg-primary-subtle' : ''}`} onClick={() => handleSelectOrder(orden.id)}>
+                <div className='scrollable-container-topLocation'>
+                  <div className='feedback-tecnicos-container align-items-center'>
+                    <h4 className='feedback-tecnicos-heading'>
+                      {orden.Cliente.nombre} {orden.Cliente.apellido}
+                    </h4>
+                  </div>
+                  <div className='feedback-tecnicos-container align-items-center'>
+                    <h4 className='feedback-tecnicos-heading'>Teléfono: {orden.Cliente.telefono}</h4>
+                  </div>
+                  <div className='feedback-tecnicos-container align-items-center'>
+                    <h4 className='feedback-tecnicos-heading'>Direccion: {orden.Cliente.direccion}</h4>
+                  </div>
+                  <div className='feedback-tecnicos-container align-items-center'>
+                    <h4 className='feedback-tecnicos-heading'>Localidad: {orden.Cliente.ubicacion}</h4>
+                  </div>
+                  <div className='feedback-tecnicos-container align-items-center'>
+                    <h4 className='feedback-tecnicos-heading'>Diagnostico: {orden.diagnostico}</h4>
+                  </div>
+                </div>
               </div>
-              <div className="feedback-tecnicos-container align-items-center">
-                <h4 className="feedback-tecnicos-heading">
-                  Teléfono: {selectedClient.telefono}
-                </h4>
-              </div>
-              <div className="feedback-tecnicos-container align-items-center">
-                <h4 className="feedback-tecnicos-heading">Calle: {calle}</h4>
-              </div>
-              <div className="feedback-tecnicos-container align-items-center">
-                <h4 className="feedback-tecnicos-heading">Altura: {altura} </h4>
-              </div>
-              <div className="feedback-tecnicos-container align-items-center">
-                <h4 className="feedback-tecnicos-heading">
-                  Localidad: {selectedClient.ubicacion}
-                </h4>
-              </div>
-            </div>
-          </div>
+            ))}
         </div>
-        <div className="col-7">
-          <div className="container-lists w-100">
-            <h2 className="px-3 pt-3 feedback-containers-heading">
-              Técnicos a cargo
-            </h2>
-            <div className="caja-excel">
-              <div className="caja-excel-wrapper px-5">
-                <table className="table">
+        <div className='col-7'>
+          <div className='container-lists w-100'>
+            <h2 className='px-3 pt-3 feedback-containers-heading'>Técnico a cargo</h2>
+            <div className='caja-excel'>
+              <div className='caja-excel-wrapper px-5'>
+                <table className='table'>
                   <tbody>
-                    <tr className="row-even">
+                    <tr className='row-even'>
                       <td>
-                        {selectedTechnician.nombre}{" "}
-                        {selectedTechnician.apellido}
+                        {selectedTechnician.nombre} {selectedTechnician.apellido}
                       </td>
                       <td>Legajo: {selectedTechnician.legajo}</td>
                       <td>Técnico a domicilio</td>
@@ -138,11 +138,8 @@ const UbicacionesOrden = () => {
             </div>
           </div>
         </div>
-        <div className="d-flex justify-content-center">
-          <button
-            className="bg-info rounded-pill text-white"
-            onClick={handleSubmit}
-          >
+        <div className='d-flex justify-content-center'>
+          <button className='bg-info rounded-pill text-white' onClick={handleSocket}>
             Confirmar
           </button>
         </div>
