98b00dadd82ad3cff45bddfe112363fd5e82a850
facuzero <facuzero_95@hotmail.com>
2024-11-26 16:35:12 -0300

    agrego boton al menu en header

--- public/electron.js
+++ public/electron.js
@@ -1,43 +1,35 @@
 const { app, BrowserWindow, screen } = require('electron');
 const path = require('path');
- 
-let mainWindow;
- 
- 
- 
-function createWindow() {
- 
-    mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
-        resizable: true,
-        maximizable: false,
-        fullscreenable: false,
-        frame: true,
-        webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: true,
-            preload: path.join(__dirname, 'preload.js')
-        }
-    });
 
-   /* mainWindow.loadFile(path.join(__dirname, '../dist', 'index.html'));*/
-   mainWindow.loadURL('http://localhost:3000')
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    resizable: true,
+    maximizable: false,
+    fullscreenable: false,
+    frame: true,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: true,
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+
+  /* mainWindow.loadFile(path.join(__dirname, '../dist', 'index.html'));*/
+  mainWindow.loadURL('http://localhost:5173');
 }
- 
- 
 
 app.whenReady().then(() => {
- 
-    createWindow();
- 
-    app.on('activate', function () {
-        if (BrowserWindow.getAllWindows().length === 0) createWindow();
-    });
+  createWindow();
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
 });
 
 app.on('window-all-closed', function () {
- 
-
-    if (process.platform !== 'darwin') app.quit();
+  if (process.platform !== 'darwin') app.quit();
 });
