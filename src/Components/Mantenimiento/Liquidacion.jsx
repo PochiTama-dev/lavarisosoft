0c0e927fec47d284b2f86c05292d448a829c66dd
facuzero <facuzero_95@hotmail.com>
2025-01-07 13:24:29 -0300

    agrego seleccion de tecnico para liquidar

--- src/Components/Mantenimiento/Liquidacion.jsx
+++ src/Components/Mantenimiento/Liquidacion.jsx
@@ -1,45 +1,41 @@
- 
-import React, { useState } from 'react';
-import './mantenimiento.css'
-import cash from '../../assets/cash-circulo.png'
-import edit from '../../assets/edit.png'
-const Liquidacion = () => {
-  const [total, setTotal] = useState( );
-  const [tecnicoHome, setTecnicoHome] = useState( );
-  const [tecnicoTaller, setTecnicoTaller] = useState( );
-  const [tecnicoEntrega, setTecnicoEntrega] = useState( );
-
-  // Funciones para manejar la edición y eliminación aquí
-
+import './mantenimiento.css';
+import { object, func } from 'prop-types';
+//import cash from '../../assets/cash-circulo.png';
+//import edit from '../../assets/edit.png';
+const Liquidacion = ({ tecnico, setModal }) => {
+  console.log(tecnico);
   const handleLiquidate = () => {
     // Lógica para manejar la liquidación
   };
 
   return (
-    <div className='liquidacion'>
-      <h1>Liquidación Alan Almendra</h1>
+    <div className='liquidacion rounded'>
+      <div className='d-flex justify-content-between'>
+        <h1>Liquidación {tecnico.nombre}</h1>
+        <h2 className='pointer' onClick={() => setModal(false)}>
+          x
+        </h2>
+      </div>
       <div className='liq-table'>
-
-     
-      <div>
-        <span>Total:</span>
-        <span>Técnico domicilio:</span>
-        <span>Técnico taller:</span>
-        <span>Técnico entrega:</span>
- 
+        <div>
+          <span>Total:</span>
+          <span>Técnico domicilio:</span>
+          <span>Técnico taller:</span>
+          <span>Técnico entrega:</span>
+        </div>
+        <div>
+          <input type='text' value={tecnico.ordenes.reduce((acumulador, orden) => acumulador + parseFloat(orden.total || 0), 0).toFixed(2)} />
+          <input type='text' />
+          <input type='text' />
+        </div>
       </div>
-      <div>
-      <input type="text" /> 
-      <input type="text" />
-      <input type="text" />
-      <input type="text" />
-      </div>
-      </div>
-      <button  onClick={handleLiquidate}>
-        Liquidar
-      </button>
+      <button onClick={handleLiquidate}>Liquidar</button>
     </div>
   );
+};
+Liquidacion.propTypes = {
+  tecnico: object,
+  setModal: func,
 };
 
 export default Liquidacion;
