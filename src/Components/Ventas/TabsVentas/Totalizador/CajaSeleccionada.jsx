24c10eac8b203ac3d7b6636acaf4c6ebcab4f531
facuzero <facuzero_95@hotmail.com>
2025-02-09 08:57:54 -0300

    cambios en totalizador

--- src/Components/Ventas/TabsVentas/Totalizador/CajaSeleccionada.jsx
+++ src/Components/Ventas/TabsVentas/Totalizador/CajaSeleccionada.jsx
@@ -1,43 +1,41 @@
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
+import { useState } from 'react';
 
 const CajaSeleccionada = ({ onDateChange }) => {
+  const [firstDate, setFirstDate] = useState();
+  const [secondDate, setSecondDate] = useState();
+
   const handleDateInput = (event) => {
+    setFirstDate(event.target.value);
     onDateChange(event.target.value);
+    console.log(firstDate);
   };
   return (
     <div>
-      <ul>
-        <li>
-          <div className="d-flex justify-content-between my-2 inputsItems">
-            <label className="text-primary" htmlFor="">
-              Filtrar por fecha
-            </label>
-            <input
-              className="rounded-pill mx-4"
-              type="date"
-              onChange={handleDateInput}
-            />
-          </div>
-        </li>
+      <div className='d-flex justify-content-evenly my-2'>
+        <label className='text-primary' htmlFor=''>
+          Filtrar por fecha
+        </label>
+        <div className='d-flex'>
+          <span>Desde</span>
+          <input className='rounded-pill mx-4' type='date' onChange={handleDateInput} />
+          <span>Hasta</span>
+          <input className='rounded-pill mx-4' type='date' onChange={handleDateInput} />
+        </div>
+      </div>
+      <ul className='row'>
+        <li className='pestañasFont pestañasInventario col text-center'>Efectivo</li>
+        <li className='pestañasFont pestañasInventario col text-center'>Dólares</li>
+        <li className='pestañasFont pestañasInventario col text-center'>Bancos</li>
       </ul>
-      <ul className="row">
-        <li className="pestañasFont pestañasInventario col text-center">
-          Efectivo
-        </li>
-        <li className="pestañasFont pestañasInventario col text-center">
-          Dólares
-        </li>
-        <li className="pestañasFont pestañasInventario col text-center">
-          Bancos
-        </li>
-      </ul>
-      <ul className="row">
-        <li className="col text-center items">ID de orden</li>
-        <li className="col text-center items">ID de repuesto</li>
-        <li className="col text-center items">Número de caja</li>
-        <li className="col text-center items">Precio</li>
-        <li className="col text-center items">Cantidad</li>
-        <li className="col text-center items">Total</li>
+      <ul className='row'>
+        <li className='col text-center items'>Periodo del Mes</li>
+        <li className='col text-center items'>Total Facturado</li>
+        <li className='col text-center items'>Total Pagado a tecnicos</li>
+        <li className='col text-center items'>Margen Bruto</li>
+        <li className='col text-center items'>Gastos Operativos</li>
+        <li className='col text-center items'>Ganancia Neta</li>
+        <li className='col text-center items'>Facturas pendientes de cobro</li>
       </ul>
     </div>
   );
--- src/Components/Ventas/TabsVentas/Totalizador/Totalizador.jsx
+++ src/Components/Ventas/TabsVentas/Totalizador/Totalizador.jsx
@@ -1,16 +1,20 @@
-import { useEffect, useState } from "react";
-import Header from "../../../Header/Header";
-import { listaCajas } from "../../../../services/cajasService";
-import Cajas from "./Cajas";
-import CajaSeleccionada from "./CajaSeleccionada";
-import DatosCaja from "./DatosCaja";
-import { listaCobros } from "../../../../services/CobrosService";
+import { useEffect, useState } from 'react';
+import Header from '../../../Header/Header';
+import { listaCajas } from '../../../../services/cajasService';
+import Cajas from './Cajas';
+import CajaSeleccionada from './CajaSeleccionada';
+import DatosCaja from './DatosCaja';
+import { listaCobros } from '../../../../services/CobrosService';
+import { useCustomContext } from '../../../../hooks/context';
 
 const Totalizador = () => {
+  const { listaFacturasCompra, listaFacturasVenta, getPresupuestos } = useCustomContext();
   const [caja, setCaja] = useState([]);
   const [cobros, setCobros] = useState([]);
-  const [selectedCajaId, setSelectedCajaId] = useState("");
-  const [selectedDate, setSelectedDate] = useState("");
+  const [selectedCajaId, setSelectedCajaId] = useState('');
+  const [selectedDate, setSelectedDate] = useState('');
+  const [totalFacturas, setTotalFacturas] = useState();
+  const [datosLiquidaciones, setDatosLiquidaciones] = useState();
 
   useEffect(() => {
     const fetchCajasData = async () => {
@@ -18,7 +22,7 @@ const Totalizador = () => {
         const data = await listaCajas();
         setCaja(data);
       } catch (error) {
-        console.error("Error fetching cajas data:", error);
+        console.error('Error fetching cajas data:', error);
         setCaja([]);
       }
     };
@@ -27,13 +31,32 @@ const Totalizador = () => {
         const data = await listaCobros();
         data !== false && setCobros(data);
       } catch (error) {
-        console.error("Error fetching cobros data:", error);
+        console.error('Error fetching cobros data:', error);
         setCobros([]);
+      }
+    };
+    const fetchFacturas = async () => {
+      try {
+        const facturasCompra = await listaFacturasCompra();
+        const facturasVenta = await listaFacturasVenta();
+        //console.log('Facturas Compras', facturasCompra);
+        //console.log('Facturas VENTAS', facturasVenta);
+        const totalCompra = facturasCompra.reduce((acumulador, factura) => acumulador + Number(factura.total), 0);
+        const totalVenta = facturasVenta.reduce((acumulador, factura) => acumulador + Number(factura.total), 0);
+        setTotalFacturas(totalCompra + totalVenta);
+      } catch (error) {
+        console.error('Error fetching cobros data:', error);
       }
     };
     fetchCajasData();
     fetchCobrosData();
+    fetchFacturas();
+    getCobrosOrdenes();
   }, []);
+
+  // GASTOS OPERATIVOS (agregar desde factura proveedores, una columna mas, true o false)
+  // GANANCIA NETA margen bruto - gastos operativos
+  // FAC PEND DE COBRO
 
   const handleCajaChange = (id) => {
     setSelectedCajaId(id);
@@ -44,29 +67,64 @@ const Totalizador = () => {
   };
 
   const filteredCobros = cobros.filter((cobro) => {
-    const matchesCaja = selectedCajaId
-      ? cobro.id_caja === selectedCajaId
-      : true;
+    const matchesCaja = selectedCajaId ? cobro.id_caja === selectedCajaId : true;
     const createdAtFormatted = cobro.created_at.slice(0, 10);
-    const matchesDate = selectedDate
-      ? createdAtFormatted === selectedDate
-      : true;
+    const matchesDate = selectedDate ? createdAtFormatted === selectedDate : true;
 
     return matchesCaja && matchesDate;
   });
 
+  const getCobrosOrdenes = async () => {
+    const cobros = await getPresupuestos();
+    //console.log(cobros);
+    const empleadosOrdenes = transformarCobrosPorEmpleado(cobros);
+    //console.log(empleadosOrdenes);
+    const totalFacturaEmpleados = empleadosOrdenes.map((empleado) => {
+      return empleado.ordenes.reduce((acumulador, orden) => acumulador + orden.total, 0);
+    });
+    const totalFacturado = totalFacturaEmpleados.reduce((acumulador, total) => parseFloat(acumulador + total, 0).toFixed(2));
+    setDatosLiquidaciones(totalFacturado);
+    //console.log(totalFacturado);
+  };
+  const transformarCobrosPorEmpleado = (cobros) => {
+    return cobros.reduce((result, cobro) => {
+      const { Empleado } = cobro.Ordene; // Extrae el empleado de la orden
+      const empleadoExistente = result.find((item) => item.empleadoId === Empleado.id);
+      const orden = {
+        ...cobro.Ordene,
+        presupuestoId: cobro.id, // Puedes agregar el ID del presupuesto si es relevante
+        PlazosReparacion: cobro.PlazosReparacion,
+        MediosDePago: cobro.MediosDePago,
+        EstadosPresupuesto: cobro.Estados_presupuesto,
+        Diagnosticos: cobro.Diagnosticos,
+        dpg: cobro.dpg,
+        total: cobro.total - (cobro.total - cobro.dpg) * cobro.Ordene.Empleado.porcentaje_arreglo,
+      };
+      if (empleadoExistente) {
+        empleadoExistente.ordenes.push(orden);
+      } else {
+        result.push({
+          empleadoId: Empleado.id,
+          nombre: `${Empleado.nombre} ${Empleado.apellido}`,
+          telefono: Empleado.telefono,
+          email: Empleado.email,
+          direccion: Empleado.direccion,
+          ordenes: [orden],
+        });
+      }
+
+      return result;
+    }, []);
+  };
+
   return (
-    <div className="totalizadorContainer">
-      <Header text="Totalizador" />
-      <div className="totalizadorLayout">
-        <Cajas
-          cajas={caja}
-          onCajaSelect={handleCajaChange}
-          selectedCajaId={selectedCajaId}
-        />
-        <div className="content">
+    <div className='totalizadorContainer'>
+      <Header text='Totalizador' />
+      <div className='totalizadorLayout'>
+        <Cajas cajas={caja} onCajaSelect={handleCajaChange} selectedCajaId={selectedCajaId} />
+        <div className='content'>
           <CajaSeleccionada onDateChange={handleDateChange} />
-          <DatosCaja cobros={filteredCobros} selectedDate={selectedDate} />
+          <DatosCaja cobros={filteredCobros} selectedDate={selectedDate} totalFacturado={totalFacturas} totalPagado={datosLiquidaciones} margenBruto={totalFacturas - datosLiquidaciones} />
         </div>
       </div>
     </div>
