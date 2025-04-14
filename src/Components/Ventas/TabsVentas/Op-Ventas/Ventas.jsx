979e410257ff02b4604864306737a3d258b1ab8e
facuzero <facuzero_95@hotmail.com>
2025-04-13 21:44:06 -0300

    remito ventas y servicios

--- src/Components/Ventas/TabsVentas/Op-Ventas/Ventas.jsx
+++ src/Components/Ventas/TabsVentas/Op-Ventas/Ventas.jsx
@@ -1,9 +1,15 @@
 import { Suspense, useState } from 'react';
 import './OpVentas.css';
+import eye from '../../../../assets/eye.svg';
+
 import PropTypes from 'prop-types';
+import jsPDF from 'jspdf';
 const Ventas = ({ data }) => {
   const [orderBy, setOrderBy] = useState(null);
   const [orderAsc, setOrderAsc] = useState(true);
+  const [modal, setModal] = useState(false);
+  const [remito, setRemito] = useState({});
+
   const mediosPagos = (num) => {
     if (num === 1) return 'Echeq';
     else if (num === 2) return 'Efectivo en dólares';
@@ -34,18 +40,59 @@ const Ventas = ({ data }) => {
       })
     : data;
 
+  const handleRemito = (item) => {
+    console.log(item);
+    const remito = {
+      fecha: new Date(item.created_at).toLocaleDateString(),
+      numero_orden: item.numero_orden,
+      nombreEmpleado: `${item.Empleado?.nombre} ${item.Empleado?.apellido}`,
+      nombreCliente: `${item.Cliente?.nombre} ${item.Cliente?.apellido}`,
+      legajo: item.Empleado?.legajo,
+      cuil: item.Cliente?.cuil,
+      monto: item.Presupuesto?.total,
+      medio_pago: mediosPagos(item.Presupuesto?.id_medio_de_pago),
+    };
+    setRemito(remito);
+    setModal(!modal);
+  };
+  const handleExportToPDF = () => {
+    const doc = new jsPDF();
+
+    // Título del remito
+    doc.setFontSize(18);
+    doc.text(`Remito Ventas y Servicios No.#${remito.numero_orden}`, 10, 20);
+
+    // Detalles del remito
+    doc.setFontSize(12);
+    doc.text(`Fecha: ${remito.fecha}`, 90, 40);
+    doc.text(`Técnico: ${remito.nombreEmpleado}`, 10, 50);
+    doc.text(`Legajo: ${remito.legajo}`, 60, 50);
+    doc.text(`Cliente: ${remito.nombreCliente}`, 10, 60);
+    doc.text(`CUIL: ${remito.cuil}`, 60, 60);
+    doc.text(`Monto: $${remito.monto}`, 10, 70);
+    doc.text(`Medio de pago: ${remito.medio_pago}`, 60, 70);
+    // Generar el PDF como un blob
+    const pdfBlob = doc.output('blob');
+
+    // Crear una URL para el blob
+    const pdfUrl = URL.createObjectURL(pdfBlob);
+
+    // Abrir el PDF en una nueva pestaña
+    window.open(pdfUrl, '_blank');
+  };
   return (
     <div className='opventas-tab-container'>
       <div>
         <div className='opventas-excel'>
-{/*           <h2 className='opventas-excel-heading'>CONSULTAR TipoOperacion</h2> */}
+          {/*           <h2 className='opventas-excel-heading'>CONSULTAR TipoOperacion</h2> */}
           <div className='opventas-excel-wrapper'>
             <table className='table'>
               <thead>
                 <tr>
                   <th onClick={() => handleSort('created_at')}>Fecha {orderBy === 'created_at' ? orderAsc ? '▲' : '▼' : <span>▼</span>}</th>
-{/*                   <th onClick={() => handleSort('operacion')}>Tipo-Operación {orderBy === 'operacion' ? orderAsc ? '▲' : '▼' : <span>▼</span>}</th>
- */}                  <th onClick={() => handleSort('numero_orden')}>N° de orden {orderBy === 'numero_orden' ? orderAsc ? '▲' : '▼' : <span>▼</span>}</th>
+                  {/*                   <th onClick={() => handleSort('operacion')}>Tipo-Operación {orderBy === 'operacion' ? orderAsc ? '▲' : '▼' : <span>▼</span>}</th>
+                   */}{' '}
+                  <th onClick={() => handleSort('numero_orden')}>N° de orden {orderBy === 'numero_orden' ? orderAsc ? '▲' : '▼' : <span>▼</span>}</th>
                   <th onClick={() => handleSort('Empleado.legajo')}>Legajo-Técnico {orderBy === 'Empleado.legajo' ? orderAsc ? '▲' : '▼' : <span>▼</span>}</th>
                   <th onClick={() => handleSort('Cliente.cuil')}>CUIL-Cliente {orderBy === 'Cliente.cuil' ? orderAsc ? '▲' : '▼' : <span>▼</span>}</th>
                   <th onClick={() => handleSort('Presupuesto.total')}>Monto {orderBy === 'Presupuesto.total' ? orderAsc ? '▲' : '▼' : <span>▼</span>}</th>
@@ -57,14 +104,22 @@ const Ventas = ({ data }) => {
                   {sortedData &&
                     sortedData.map((item, index) => (
                       <tr key={index} className={index % 2 === 0 ? '' : 'row-even'}>
-                        <td>{new Date(item.created_at).toLocaleDateString()}</td><td>{item.numero_orden}</td><td>{item.Empleado?.legajo}</td><td>{item.Cliente?.cuil}</td><td>$ {item.Presupuesto?.total}</td><td>{mediosPagos(item.Presupuesto?.id_medio_de_pago)}</td>
+                        <td>{new Date(item.created_at).toLocaleDateString()}</td>
+                        <td>{item.numero_orden}</td>
+                        <td>{item.Empleado?.legajo}</td>
+                        <td>{item.Cliente?.cuil}</td>
+                        <td>$ {item.Presupuesto?.total}</td>
+                        <td>{mediosPagos(item.Presupuesto?.id_medio_de_pago)}</td>
+                        <td>
+                          <img className='pointer' style={{ width: '30px' }} src={eye} alt='Ver remito' onClick={() => handleRemito(item)} />
+                        </td>
                       </tr>
                     ))}
                 </Suspense>
               </tbody>
             </table>
           </div>
-  {/*         <div className='opventas-export-button-container'>
+          {/*         <div className='opventas-export-button-container'>
             <button className='opventas-export-button' type='submit'>
               <svg width='34' height='41' viewBox='0 0 34 41' fill='none' xmlns='http://www.w3.org/2000/svg'>
                 <path
@@ -79,6 +134,34 @@ const Ventas = ({ data }) => {
               </svg>
             </button>
           </div> */}
+          {modal && (
+            <div className='remito-modal'>
+              <div className='remito-content'>
+                <div style={{ width: '50%' }} className='d-flex justify-content-evenly'>
+                  <h2>Remito #{remito.numero_orden}</h2>
+                  <h4 className='pointer' onClick={() => setModal(!modal)}>
+                    X
+                  </h4>
+                </div>
+                <h4>Fecha: {remito.fecha}</h4>
+                <div style={{ width: '50%' }} className='d-flex justify-content-between'>
+                  <p>Técnico: {remito.nombreEmpleado}</p>
+                  <p>Legajo: {remito.legajo}</p>
+                </div>
+                <div style={{ width: '50%' }} className='d-flex justify-content-between'>
+                  <p>Cliente: {remito.nombreCliente}</p>
+                  <p>CUIL: {remito.cuil}</p>
+                </div>
+                <div style={{ width: '50%' }} className='d-flex justify-content-between'>
+                  <p>Monto: $ {remito.monto}</p>
+                  <p>Medio de pago: {remito.medio_pago}</p>
+                </div>
+                <button className='pointer m-5' onClick={handleExportToPDF}>
+                  Imprimir
+                </button>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </div>
